2d64d563a2e30f3cf6cee6333caa0f0fcf37702a
yeepixel <jujinho2231@gmail.com>
2024-09-30 06:50:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to 🍵77STORE2K.ID"];
+const statusMessages = ["Listening to 77STORE2K.ID"];
 const statusTypes = [ 'dnd' ];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
